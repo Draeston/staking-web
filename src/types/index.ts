a9ff1c7402c04616016942760031ed3a6919ae08
Nikita Morozov <69845877+Draeston@users.noreply.github.com>
2024-03-17 18:39:23 +0300

    Initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Pool = {
+  contract: string;
+  token: string;
+  rewardToken: string;
+  reward: number;
+  stake: number;
+  tokens: number;
+  claimed: number;
+  rewards: number;
+  endTime: number;
+  claimedBy: number;
+  stakedBy: number;
+  rewardsBy: number;
+};
